b1dcbc68e0446f5a5802d0b1010fd8dab6526412
annave123 <tony.nave@aol.com>
2016-11-01 22:45:22 -1000

    Hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        String ss[] = {"Bill", "Mary", "Lee", "Agnes", "Alfred", "Thomas", "Alvin", "Bernard", "Ezra", "Herman"};
+        for (int i = 0; i < ss.length; i++) {
+            ss[i].equals(ss);
+        }
+        Arrays.sort(ss);
+        for (int i = 0; i < ss.length; i++) {
+            System.out.println(ss[i]);
+
+
+        }
     }
 }
